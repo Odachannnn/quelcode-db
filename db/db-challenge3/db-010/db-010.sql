7127474b8854b63915b10a2ecebc01f80a59f4f2
Asako Oda <12odadao14@gmail.com>
2020-07-06 13:54:04 +0900

    add DB課題db-010

---
+++ db/db-challenge3/db-010/db-010.sql
@@ -0,0 +1,10 @@
+SELECT u.name AS ユーザー名, MAX(p.posted_at) AS 投稿日時, c.name AS チャットルーム名
+FROM posts p
+JOIN users u
+ON p.posted_by = u.id
+JOIN chats c
+ON p.chat_id = c.id
+WHERE u.is_deleted = 0
+AND c.is_deleted = 0
+GROUP BY c.id
+ORDER BY c.id;
