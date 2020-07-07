82df5a2cf3c8c3bb024a8a18e8da4707510a17ba
Asako Oda <12odadao14@gmail.com>
2020-07-07 15:10:01 +0900

    update db-010:要件を満たすよう修正及びそのキャプチャ

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,10 +1,14 @@
-SELECT u.name AS ユーザー名, MAX(p.posted_at) AS 投稿日時, c.name AS チャットルーム名
-FROM posts p
+SELECT u.name AS ユーザー名, p1.posted_at AS 最新投稿日時, c.name AS チャット名
+FROM posts p1
+JOIN
+(SELECT MAX(posted_at) AS posted_at, chat_id
+FROM posts
+WHERE is_deleted = 0
+GROUP BY chat_id
+) AS p2
+ON p1.chat_id = p2.chat_id
+AND p1.posted_at = p2.posted_at
 JOIN users u
-ON p.posted_by = u.id
+ON p1.posted_by= u.id
 JOIN chats c
-ON p.chat_id = c.id
-WHERE u.is_deleted = 0
-AND p.is_deleted = 0
-GROUP BY c.id
-ORDER BY c.id;
+ON p1.chat_id = c.id;
