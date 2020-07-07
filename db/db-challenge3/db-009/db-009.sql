cc776f85ed6aac1087a08278576fea74ad5f8d0e
Asako Oda <12odadao14@gmail.com>
2020-07-07 13:28:49 +0900

    update db-009：削除された投稿について条件句の修正、及びキャプチャ変更

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -4,7 +4,7 @@ JOIN chats c
 ON p.chat_id = c.id
 JOIN users u
 ON p.posted_by = u.id
-WHERE c.is_deleted = 0
+WHERE p.is_deleted = 0
 AND u.is_deleted = 0
 GROUP BY c.id
 ORDER BY COUNT(p.id) DESC;
